535819869b0fc3e2e5817f6c3e1e46e06f62c453
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2016-12-27 15:14:49 -0800

    tnag doesn't need to run as root

--- tnag.sh
+++ tnag.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# es_opt script. Python wrapper for es_opt.py
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
-
 RUN_LOC="/opt/telegram_nagios"
 
 pushd ${RUN_LOC} &> /dev/null
